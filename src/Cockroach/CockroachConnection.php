f63200e1d6bc1e92d51dd28c594f88e56fe9c10a
Nikolaj Boel Jensen <nbj@cego.dk>
2017-07-12 12:42:18 +0200

    Add initial classes

---
+++ src/Cockroach/CockroachConnection.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Cockroach;
+
+use Doctrine\DBAL\Driver\PDOPgSql\Driver as DoctrineDriver;
+use App\Cockroach\Query\CockroachGrammar as QueryGrammar;
+use App\Cockroach\Schema\CockroachGrammar as SchemaGrammar;
+use Illuminate\Database\Connection;
+
+class CockroachConnection extends Connection
+{
+    /**
+     * Get the default query grammar instance.
+     *
+     * @return \App\Cockroach\Query\CockroachGrammar
+     */
+    protected function getDefaultQueryGrammar()
+    {
+        return $this->withTablePrefix(new QueryGrammar);
+    }
+
+    /**
+     * Get a schema builder instance for the connection.
+     *
+     * @return \Illuminate\Database\Schema\PostgresBuilder
+     */
+    public function getSchemaBuilder()
+    {
+        if (is_null($this->schemaGrammar)) {
+            $this->useDefaultSchemaGrammar();
+        }
+
+        return new CockroachBuilder($this);
+    }
+
+    /**
+     * Get the default schema grammar instance.
+     *
+     * @return \App\Cockroach\Schema\CockroachGrammar
+     */
+    protected function getDefaultSchemaGrammar()
+    {
+        return $this->withTablePrefix(new SchemaGrammar);
+    }
+
+    /**
+     * Get the default post processor instance.
+     *
+     * @return \Illuminate\Database\Query\Processors\PostgresProcessor
+     */
+    protected function getDefaultPostProcessor()
+    {
+        return new CockroachProcessor();
+    }
+
+    /**
+     * Get the Doctrine DBAL driver.
+     *
+     * @return \Doctrine\DBAL\Driver\PDOPgSql\Driver
+     */
+    protected function getDoctrineDriver()
+    {
+        return new DoctrineDriver;
+    }
+}
---
+++ src/Cockroach/CockroachServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Cockroach;
+
+use Illuminate\Database\Connection;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+
+class CockroachServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+        Connection::resolverFor('cockroach', function ($connection, $database, $prefix, $config) {
+            $connector = new CockroachConnector();
+            $connection = $connector->connect($config);
+            $db = new CockroachConnection($connection, $database, $prefix, $config);
+
+
+            return $db;
+        });
+    }
+}
